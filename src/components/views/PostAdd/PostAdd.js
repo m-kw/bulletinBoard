1097bde294c31c28941f1f770f8458ab31faef28
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2020-03-26 23:49:28 +0100

    Read data of new post, save it in state

--- src/components/views/PostAdd/PostAdd.js
+++ src/components/views/PostAdd/PostAdd.js
@@ -3,18 +3,25 @@ import PropTypes from 'prop-types';
 
 import Container from '@material-ui/core/Container';
 import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
 import clsx from 'clsx';
+import shortid from 'shortid';
 
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
 
 import styles from './PostAdd.module.scss';
-import { CardHeader } from '@material-ui/core';
 
 const Component = ({ className }) => {
+
+  const today = new Date();
+  const day = today.getDate();
+  const month = today.getMonth();
+  const year = today.getFullYear();
+  const date = day + '/' + month + '/' + year;
 
   const titleProps = {
     minLength: 10,
@@ -24,22 +31,77 @@ const Component = ({ className }) => {
     minLength: 20,
   };
 
+  const [post, setPost] = React.useState({
+    id: shortid.generate(),
+    date: date,
+  });
+
   const handleSubmit = e => {
     e.preventDefault();
     console.log('Submitted');
   };
 
+  const handleChange = (event, name) => {
+    setPost({
+      ...post,
+      [name]: event.target.value,
+    });
+  };
+
+  console.log('post', post);
+
   return (
     <div className={clsx(className, styles.root)}>
       <Container maxWidth="lg">
+
         <Card className={styles.card}>
+
           <CardHeader title="Add new post" />
+
           <form className={styles.form} autoComplete="off" onSubmit={e => handleSubmit(e)}>
-            <TextField id="title" label="Title" inputProps={titleProps} required />
-            <TextField id="price" label="Price" />
-            <TextField variant="outlined" multiline id="content" inputProps={contentProps} label="Content" placeholder="Write your post here" rows="10" required />
-            <TextField id="mail" label="E-mail" type="email" required />
-            <TextField id="phone" label="Phone number" />
+            <TextField
+              id="title"
+              label="Title"
+              required
+              inputProps={titleProps}
+              value={post.title}
+              onChange={e => handleChange(e, 'title')}
+            />
+            <TextField
+              id="price"
+              label="Price"
+              type="number"
+              value={post.price}
+              onChange={e => handleChange(e, 'price')}
+            />
+            <TextField
+              variant="outlined"
+              multiline
+              id="content"
+              inputProps={contentProps}
+              label="Content"
+              placeholder="Write your post here"
+              rows="10"
+              required
+              value={post.content}
+              onChange={e => handleChange(e, 'content')}
+
+            />
+            <TextField
+              id="mail"
+              label="E-mail"
+              type="email"
+              required
+              value={post.mail}
+              onChange={e => handleChange(e, 'mail')}
+            />
+            <TextField
+              id="phone"
+              label="Phone number"
+              type="number"
+              value={post.phone}
+              onChange={e => handleChange(e, 'phone')}
+            />
             <Button type="submit" color="secondary" variant="contained" className={styles.button}>Save</Button>
             <Button color="secondary" href="/" variant="contained" className={styles.button}>Return</Button>
           </form>
