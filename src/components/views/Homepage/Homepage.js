3dc7eb32661a949b6e5ef60b91b2beeff9164574
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2020-03-27 14:06:56 +0100

    Add link button to each post in Homepage view

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -6,6 +6,8 @@ import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
 
 import clsx from 'clsx';
 
@@ -41,8 +43,14 @@ class Component extends React.Component {
           {posts.map(el => (
             <Card key={el.id} className={styles.card}>
               <CardHeader title={el.title} subheader={`${el.date}/${el.updateDate}`} />
+              <CardActions className={styles.link}>
+                <Button size="small" color="secondary" variant="contained" href={`/post/${el.id}`}>
+                  Show details
+                </Button>
+              </CardActions>
             </Card>
           ))}
+
         </Container>
         {children}
       </div>
