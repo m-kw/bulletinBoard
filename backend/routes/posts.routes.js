ffc6bd26c71876cb2393935ee5f0bc1526a61d5d
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2020-04-01 12:13:47 +0200

    Create endpoint to add new post, update Post model to validate length of the title and content

--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const sanitize = require('mongo-sanitize');
 
 const Post = require('../models/post.model');
 
 router.get('/posts', async (req, res) => {
+
   try {
     const result = await Post
       .find({ status: 'published' })
@@ -25,6 +27,33 @@ router.get('/posts/:id', async (req, res) => {
     else res.json(result);
   }
   catch(err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/posts', async (req, res) => {
+
+  try {
+    const cleanedBody = sanitize(req.body);
+    const { author, created, updated, status, title, content, image, price, phone, location } = cleanedBody;
+
+    const newPost = new Post({
+      author: author,
+      created: created,
+      updated: updated,
+      status: status,
+      title: title,
+      content: content,
+      image: image,
+      price: price,
+      phone: phone,
+      location: location,
+    });
+
+    await newPost.save();
+    res.json(newPost);
+  }
+  catch (err) {
     res.status(500).json(err);
   }
 });
