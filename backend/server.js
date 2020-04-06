75e6d1c8fe2d3198776c14c8e360d94044102aef
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2020-04-06 17:58:25 +0200

    Install passport, auth0, add their config to server.js

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,59 @@ const path = require('path');
 const mongoose = require('mongoose');
 const formidable = require('express-formidable');
 const shortid = require('shortid');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
 
 const postsRoutes = require('./routes/posts.routes');
 
 const app = express();
+
+/* EXPRESS-SESSION CONFIG */
+const sess = {
+  secret: 'Custom Secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+};
+
+if (app.get('env') === 'production') {
+  sess.cookie.secure = true;
+}
+
+// Uncomment the line below if your application is behind a proxy (like on Heroku)
+// or if you're encountering the error message:
+// "Unable to verify authorization request state"
+// app.set('trust proxy', 1);
+
+app.use(session(sess));
+
+/* PASSPORT & AUTH0 CONFIG */
+dotenv.config();
+
+const strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL: 'http://localhost:3000',
+  },
+  function (accessToken, refreshToken, extraParams, profile, done) {
+    return done(null, profile);
+  }
+);
+
+passport.use(strategy);
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 /* MIDDLEWARE */
 app.use(cors());
